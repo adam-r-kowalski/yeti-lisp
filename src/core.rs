4e1d38cdd7d4327d9e22580a1b7f8f2beb0dfe99
Adam Kowalski <adam.kowalski.work@gmail.com>
2023-10-10 00:52:49 -0700

    - * /

--- src/core.rs
+++ src/core.rs
@@ -1,12 +1,31 @@
 use crate::Expression;
-use crate::Expression::{Integer, IntrinsicFunction};
+use crate::Expression::{Integer, IntrinsicFunction, Ratio};
 use im::{hashmap, HashMap};
+use rug::Rational;
 
 pub fn environment() -> HashMap<String, Expression> {
     hashmap! {
         "+".to_string() => IntrinsicFunction(
           |arguments| match (&arguments[0], &arguments[1]) {
             (Integer(lhs), Integer(rhs)) => Integer((lhs + rhs).into()),
+            _ => panic!("Expected integer argument"),
+          }
+        ),
+        "-".to_string() => IntrinsicFunction(
+          |arguments| match (&arguments[0], &arguments[1]) {
+            (Integer(lhs), Integer(rhs)) => Integer((lhs - rhs).into()),
+            _ => panic!("Expected integer argument"),
+          }
+        ),
+        "*".to_string() => IntrinsicFunction(
+          |arguments| match (&arguments[0], &arguments[1]) {
+            (Integer(lhs), Integer(rhs)) => Integer((lhs * rhs).into()),
+            _ => panic!("Expected integer argument"),
+          }
+        ),
+        "/".to_string() => IntrinsicFunction(
+          |arguments| match (&arguments[0], &arguments[1]) {
+            (Integer(lhs), Integer(rhs)) => Ratio(Rational::from((lhs, rhs))),
             _ => panic!("Expected integer argument"),
           }
         ),
