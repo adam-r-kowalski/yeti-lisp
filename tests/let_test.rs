12b2d52a0617108636235a3c3cf51fbd7015a1e2
Adam Kowalski <adam.kowalski.work@gmail.com>
2023-10-29 16:51:13 -0700

    two let bindings

--- tests/let_test.rs
+++ tests/let_test.rs
@@ -13,3 +13,14 @@ fn let_binding() -> Result {
     assert_eq!(actual, expected);
     Ok(())
 }
+
+#[test]
+fn two_let_binding() -> Result {
+    let tokens = yeti::Tokens::from_str("(let [a 5 b 10] (+ a b))");
+    let expression = yeti::parse(tokens);
+    let environment = yeti::core::environment();
+    let (_, actual) = yeti::evaluate(environment, expression)?;
+    let expected = yeti::Expression::Integer(Integer::from(15));
+    assert_eq!(actual, expected);
+    Ok(())
+}
