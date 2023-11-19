a1d7d5d3a0ef6f80ef419ec9cb8699dfc03345b5
Adam Kowalski <adamkowalski@Adams-MacBook-Pro.local>
2023-11-18 23:13:26 -0800

    json from string

--- tests/json_test.rs
+++ tests/json_test.rs
@@ -5,17 +5,23 @@ type Result = std::result::Result<(), yeti::effect::Effect>;
 #[tokio::test]
 async fn json_to_string_for_map() -> Result {
     let env = yeti::core::environment();
-    let (_, actual) = yeti::evaluate_source(
-        env,
-        r#"
-        (json/to-string
-         {:first "John"
-          :last "Smith"})
-    "#,
-    )
-    .await?;
+    let (_, actual) =
+        yeti::evaluate_source(env, r#"(json/to-string {:first "John" :last "Smith"})"#).await?;
     let expected =
         yeti::Expression::String("{\n  \"first\": \"John\",\n  \"last\": \"Smith\"\n}".to_string());
+    assert_eq!(actual, expected);
+    Ok(())
+}
+
+#[tokio::test]
+async fn json_from_string_for_map() -> Result {
+    let env = yeti::core::environment();
+    let (env, actual) = yeti::evaluate_source(
+        env,
+        r#"(json/from-string "{\n  \"first\": \"John\",\n  \"last\": \"Smith\"\n}")"#,
+    )
+    .await?;
+    let (_, expected) = yeti::evaluate_source(env, r#"{:first "John" :last "Smith"}"#).await?;
     assert_eq!(actual, expected);
     Ok(())
 }
