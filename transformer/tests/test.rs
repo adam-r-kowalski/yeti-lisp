307745559a21acdf3a4298198747fed97ff577d7
Adam Kowalski <adam.kowalski.work@gmail.com>
2023-12-19 21:29:13 -0800

    able to load a model utilizing candle

--- transformer/tests/test.rs
+++ transformer/tests/test.rs
@@ -1,7 +1,12 @@
 use candle_core::{Device, Shape, Tensor};
+use candle_transformers::models::quantized_mistral;
+use tokenizers::tokenizer::Tokenizer;
+
+pub type Error = Box<dyn std::error::Error + Send + Sync>;
+pub type Result<T> = std::result::Result<T, Error>;
 
 #[test]
-fn matmul_two_tensors_with_cpu() -> Result<(), Box<dyn std::error::Error>> {
+fn matmul_two_tensors_with_cpu() -> Result<()> {
     let device = Device::Cpu;
     let a = Tensor::arange(0f32, 6f32, &device)?.reshape((2, 3))?;
     let b = Tensor::arange(0f32, 6f32, &device)?.reshape((3, 2))?;
@@ -12,11 +17,35 @@ fn matmul_two_tensors_with_cpu() -> Result<(), Box<dyn std::error::Error>> {
 
 #[cfg(feature = "metal")]
 #[test]
-fn matmul_two_tensors_with_metal() -> Result<(), Box<dyn std::error::Error>> {
+fn matmul_two_tensors_with_metal() -> Result<()> {
     let device = Device::new_metal(0)?;
     let a = Tensor::arange(0f32, 6f32, &device)?.reshape((2, 3))?;
     let b = Tensor::arange(0f32, 6f32, &device)?.reshape((3, 2))?;
     let c = a.matmul(&b)?;
     assert_eq!(c.shape(), &Shape::from((2, 2)));
+    Ok(())
+}
+
+#[test]
+fn tokenize_prompt() -> Result<()> {
+    let path = "/Users/adamkowalski/code/mistral7b/tokenizer.json";
+    let tokenizer = Tokenizer::from_file(path)?;
+    let prompt = "Hello, world!";
+    let add_special_tokens = true;
+    let tokens = tokenizer
+        .encode(prompt, add_special_tokens)?
+        .get_tokens()
+        .to_vec();
+    assert_eq!(tokens, vec!["<s>", "▁Hello", ",", "▁world", "!"]);
+    Ok(())
+}
+
+#[test]
+fn load_model() -> Result<()> {
+    let path = "/Users/adamkowalski/code/mistral7b/model-q8_0.gguf";
+    let use_flash_attn = true;
+    let config = quantized_mistral::Config::config_7b_v0_1(use_flash_attn);
+    let vb = quantized_mistral::VarBuilder::from_gguf(path)?;
+    let model = quantized_mistral::Model::new(&config, vb)?;
     Ok(())
 }
